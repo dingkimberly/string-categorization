fe4bbdff3ee903e66eb29f6343907739c15ebb3d
Mason Tian <mtian@princeton.edu>
2018-12-01 05:23:52 -0500

    working global error counter

--- backend/cos_sim.py
+++ backend/cos_sim.py
@@ -4,18 +4,33 @@ from sklearn import random_projection
 from sklearn.metrics.pairwise import cosine_similarity
 import functools
 import time
-from multiprocessing import Pool, cpu_count
+from multiprocessing import Pool, cpu_count, Value
 
-total = 0
-errors = 0
 
-def hyperplanes(X):
-    transformer = random_projection.GaussianRandomProjection()
-    print(X.shape)
-    X_new = transformer.fit_transform(X)
-    print(X_new.shape)
 
-    return X_new
+# def hyperplanes(X):
+#     transformer = random_projection.GaussianRandomProjection()
+#     print(X.shape)
+#     X_new = transformer.fit_transform(X)
+#     print(X_new.shape)
+
+#     return X_new
+
+class Counter(object):
+    def __init__(self):
+        self.e = Value('i', 0)
+        self.t = Value('i', 0)
+
+    def inc_error(self):
+        with self.e.get_lock():
+            self.e.value += 1
+
+    def inc_total(self):
+        with self.t.get_lock():
+            self.t.value += 1
+
+    def print_error(self):
+        print(self.e.value/self.t.value*100,"% error.")
 
 
 def cosine_similarity_wrap(vec_to_compare):
@@ -39,14 +54,19 @@ def cosine_similarity_pool_map(X, Y, Xtest, params):
             max_sim_indexes.append(i)
 
     cats = set(list(map(lambda x: Y[x,0], max_sim_indexes)))
-    print(cats, params[1])
+    
+    global count
+    count.inc_total()
+
     if params[1] not in cats:
+        count.inc_error()
+        count.print_error()
         return False
+
     return True
 
 def pooled_test(X, Y, Xtest, Ytest):
     csp = csp_partial(X,Y,Xtest)
-
     p = Pool(cpu_count())
     correct_list = p.map(csp, list(zip(list(range(Xtest.shape[0])), \
             map(lambda x: x[0],Ytest.tolist()))))
@@ -95,6 +115,8 @@ def t():
 
 
 if __name__ == "__main__":
+    global count
+    count = Counter()
     xtrain, xtest, ytrain, ytest = process()
     X, Y, index_to_word, word_to_index = make_vectors(xtrain, ytrain)
     Xtest, Ytest = make_test_vectors(xtest,ytest, word_to_index) 
