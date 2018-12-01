102502b77d14bcfcc2fac6fef658da6107ad67c8
Mason Tian <mtian@princeton.edu>
2018-12-01 09:06:07 -0500

    clustering experiment

--- backend/cos_sim.py
+++ backend/cos_sim.py
@@ -86,30 +86,30 @@ def t():
 
 
 
-# def test(X, Y, Xtest, Ytest):
-#     errors = 0
+def test(X, Y, Xtest, Ytest):
+    errors = 0
 
-#     for i1, current_row in enumerate(Xtest):
-#         cos_sim = cosine_similarity_wrap(current_row)
+    for i1, current_row in enumerate(Xtest):
+        cos_sim = cosine_similarity_wrap(current_row)
         
-#         max_sim = 0
-#         max_sim_indexes = []
+        max_sim = 0
+        max_sim_indexes = []
 
-#         for i2, row in enumerate(X):
-#             sim = cos_sim(row)
-#             if sim[0,0] > max_sim:
-#                 max_sim = sim[0,0]
-#                 max_sim_indexes = [i2]
-#             elif sim[0,0] == max_sim:
-#                 max_sim_indexes.append(i2)
+        for i2, row in enumerate(X):
+            sim = cos_sim(row)
+            if sim[0,0] > max_sim:
+                max_sim = sim[0,0]
+                max_sim_indexes = [i2]
+            elif sim[0,0] == max_sim:
+                max_sim_indexes.append(i2)
         
-#         t()
-#         print((i1*X.shape[0]+i2)/(X.shape[0]*Xtest.shape[0])*100, "%")
-#         cats = set(list(map(lambda x: Y[x,0], max_sim_indexes)))
-#         if Ytest[i1,0] not in cats:
-#             errors += 1
-#             print("errors: ", errors/(i1+1)*100, "%")
-#     return 
+        t()
+        print((i1*X.shape[0]+i2)/(X.shape[0]*Xtest.shape[0])*100, "%")
+        cats = set(list(map(lambda x: Y[x,0], max_sim_indexes)))
+        if Ytest[i1,0] not in cats:
+            errors += 1
+            print("errors: ", errors/(i1+1)*100, "%")
+    return 
 
 
 
@@ -119,5 +119,37 @@ if __name__ == "__main__":
     count = Counter()
     xtrain, xtest, ytrain, ytest = process()
     X, Y, index_to_word, word_to_index = make_vectors(xtrain, ytrain)
+    
+    from collections import defaultdict
+    y_dict = defaultdict(list)
+    for i, y in enumerate(Y):
+        ystr = y[0,0]
+        if ystr in y_dict:
+            y_dict[ystr].append(i)
+        else:
+            y_dict[ystr] = [i]
+
+    print(X)
+
+    cat_means = []
+    cat_name = []
+    import math
+    from sklearn.cluster import KMeans
+    for key in y_dict:
+        Xsub = np.take(X.todense(),y_dict[key],axis=0)
+        km = KMeans(n_clusters = math.ceil(Xsub.shape[0]/10))
+        km.fit(Xsub)
+        for center in km.cluster_centers_:
+            cat_means.append(center)
+            cat_name.append(key)
+
+    cat_means = np.matrix(np.vstack(cat_means))
+    cat_name = np.matrix(np.vstack(cat_name))
+
     Xtest, Ytest = make_test_vectors(xtest,ytest, word_to_index) 
-    pooled_test(X, Y, Xtest, Ytest)
+
+    test(cat_means, cat_name, Xtest, Ytest)
+
+
+
+        # pooled_test(X, Y, Xtest, Ytest)
