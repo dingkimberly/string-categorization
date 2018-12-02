26520057bc8a62ba492d961c1d553fad6bda35b2
Mason Tian <mtian@princeton.edu>
2018-12-01 22:39:20 -0500

    dimension reduction code

--- backend/dimension_reduction.py
+++ backend/dimension_reduction.py
@@ -134,11 +134,18 @@ def test(X, Y, Xtest, Ytest):
     return 
 
 if __name__ == "__main__":
+    from sklearn.decomposition import TruncatedSVD
+    
     global count
     count = Counter()
     xtrain, xtest, ytrain, ytest = process()
-    X, Y, word_to_index = make_vectors(xtrain, ytrain)
+    X, Y, word_to_index = make_vectors(xtrain, ytrain) 
+
+    svd = TruncatedSVD(n_components=100, n_iter=5, random_state=0)
+
+    Xred = svd.fit_transform(X)
     Xtest, Ytest = make_test_vectors(xtest,ytest, word_to_index) 
-    test(X, Y, Xtest, Ytest)
+    Xtest_red = svd.transform(Xtest)
+    test(Xred, Y, Xtest_red, Ytest)
 
 
